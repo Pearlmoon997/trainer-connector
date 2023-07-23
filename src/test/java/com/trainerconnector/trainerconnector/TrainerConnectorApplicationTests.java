8edbc9db942f84fe8714517e1204474d195cbdb0
Pearlmoon997 <pearlmoon997@gmail.com>
2023-07-23 14:59:10 +0900

    INIT

---
+++ src/test/java/com/trainerconnector/trainerconnector/TrainerConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainerconnector.trainerconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
